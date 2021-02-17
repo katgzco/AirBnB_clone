24d22173db57eae11ea0722a0fc7cc882506bb89
katgzco <katgzco@gmail.com>
2021-02-17 10:54:50 -0500

    Doc - add documentation for module

--- console.py
+++ console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from cmd import Cmd
+""" Module for Console """
+import cmd
 from models.base_model import BaseModel
 from models.user import User
 from models.city import City
@@ -11,7 +12,7 @@ from models import storage
 import shlex
 
 
-class HBNBCommand(Cmd):
+class HBNBCommand(cmd.Cmd):
 
     __dict_class = {"BaseModel": BaseModel, "User": User,
                     "City": City, "State": State, "Place": Place,
@@ -20,19 +21,23 @@ class HBNBCommand(Cmd):
     prompt = "(hbnb) "
 
     def do_EOF(self, line):
-        """ to exit the program"""
+        """Exit command to exit the program
+        """
         return True
 
     def do_quit(self, line):
-        """ to exit the program """
+        """Quit command to exit the program
+        """
         return True
 
     def do_emptyline(self, line):
-        """ an empty line + ENTER shouldn’t execute anything """
+        """an empty line + ENTER shouldn’t execute anything
+        """
         pass
 
     def do_create(self, line):
-        """ Creates a new instance of the class """
+        """Creates a new instance of the class
+        """
         line_tokenized = HBNBCommand.do_manage(line)
         if line_tokenized:
             if not line_tokenized[0] in HBNBCommand.__dict_class:
@@ -42,12 +47,12 @@ class HBNBCommand(Cmd):
                 instance = HBNBCommand.__dict_class[line_tokenized[0]]()
                 storage.save()
                 print(instance.id)
+
         else:
             print("** class name missing **")
 
     def do_show(self, line):
-        """
-        Prints the string representation of an instance
+        """Prints the string representation of an instance
         based on the class name
         """
         line_tokenized = HBNBCommand.do_manage(line)
@@ -72,7 +77,8 @@ class HBNBCommand(Cmd):
             print("** class name missing **")
 
     def do_destroy(self, line):
-        """ Deletes an instance based on the class name and id """
+        """Deletes an instance based on the class name and id
+        """
         line_tokenized = HBNBCommand.do_manage(line)
 
         if line_tokenized:
@@ -96,8 +102,7 @@ class HBNBCommand(Cmd):
             print("** class name missing **")
 
     def do_all(self, line):
-        """
-        Prints all string representation of all
+        """Prints all string representation of all
         instances based or not on the class name
         """
         line_tokenized = HBNBCommand.do_manage(line)
@@ -119,8 +124,7 @@ class HBNBCommand(Cmd):
         print(list_objects)
 
     def do_update(self, line):
-        """
-        Updates an instance based on the class name and
+        """Updates an instance based on the class name and
         id by adding or updating attribute
         """
         line_tokenized = HBNBCommand.do_manage(line)
@@ -153,11 +157,13 @@ class HBNBCommand(Cmd):
             instance = dictionary[key].__dict__
             line_tokenized[3] = str(line_tokenized[3])
             instance[line_tokenized[2]] = line_tokenized[3]
+            instance.save()
         else:
             print("** class name missing **")
 
-    def manage(line):
-        """ splits the line arguments splits and return an array """
+    def do_manage(line):
+        """splits the line arguments splits and return an array
+        """
         tokens = shlex.split(line)
         return list(tokens)
 
