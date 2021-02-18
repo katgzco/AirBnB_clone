d292cbf136817de3a00490076e030a23197cc6f4
Giraluna1 <lunagolo@hotmail.com>
2021-02-17 21:52:43 -0500

    Add first test for the class

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Module Unittest for BaseModel """
+
+import pep8
+from unittest import TestCase
+from models.base_model import BaseModel
+
+
+class TestBaseModel(TestCase):
+    """ test class BaseModel """
+
+    def test_A_pep8_conformance(self):
+        """Test that we conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files([
+            'models/base_model.py', 'tests/test_models/test_base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_module_doc(self):
+        """ check for module documentation """
+        self.assertTrue(len(BaseModel.__doc__) > 0)
+
+    def test_class_doc(self):
+        """ check for documentation """
+        self.assertTrue(len(BaseModel.__doc__) > 0)
+
+    def test_method_docs(self):
+        """ check for method documentation """
+        for func in dir(BaseModel):
+            self.assertTrue(len(func.__doc__) > 0)
