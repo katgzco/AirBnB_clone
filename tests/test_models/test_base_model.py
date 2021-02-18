4400d1b2ff456f0c56465a62c8d5809644c68914
Giraluna1 <lunagolo@hotmail.com>
2021-02-17 22:19:43 -0500

    Add only pep8

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -16,16 +16,3 @@ class TestBaseModel(TestCase):
             'models/base_model.py', 'tests/test_models/test_base_model.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
-
-    def test_module_doc(self):
-        """ check for module documentation """
-        self.assertTrue(len(BaseModel.__doc__) > 0)
-
-    def test_class_doc(self):
-        """ check for documentation """
-        self.assertTrue(len(BaseModel.__doc__) > 0)
-
-    def test_method_docs(self):
-        """ check for method documentation """
-        for func in dir(BaseModel):
-            self.assertTrue(len(func.__doc__) > 0)
